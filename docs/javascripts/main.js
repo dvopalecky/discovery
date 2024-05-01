cf5d85c02eb809cf97c899642fa3654920e67fbb
Daniel Vopalecky <daniel.vopalecky@gmail.com>
2024-05-01 16:29:08 +0200

    hide search and detailed navigation menu

--- docs/javascripts/main.js
+++ docs/javascripts/main.js
@@ -1,28 +1,32 @@
-const thirdsLabels = [
+function setupSwiper() {
+  const thirdsLabels = [
     "Pohled zpět",
     "Pohled nahoru",
     "Pohled dopředu"
-]
+  ]
 
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + thirdsLabels[index] + "</span>";
+  const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      loop: true,
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + thirdsLabels[index] + "</span>";
+          },
         },
-      },
 
-    breakpoints: {
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 20
-        }
-    }
+      breakpoints: {
+          768: {
+              slidesPerView: 3,
+              spaceBetween: 20
+          }
+      }
+    });
+
+  swiper.on('slideChange', function () {
+    // console.log(swiper.activeIndex);
   });
+}
 
-swiper.on('slideChange', function () {
-  console.log(swiper.activeIndex);
-});
+document.addEventListener('DOMContentLoaded', setupSwiper);
