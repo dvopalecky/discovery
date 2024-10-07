f42482fe18d413c81c4f415a42ef1d9ca3ae0b1d
Daniel Vopalecky <daniel.vopalecky@gmail.com>
2024-10-07 11:11:05 +0200

    Add Krokysjezisem visual tests

--- test.sh
+++ test.sh
@@ -12,16 +12,16 @@ bunx playwright test tests/discovery.spec.js
 echo "Killing server"
 kill $SERVER_PID
 
-# echo "Building Krokysjezisem"
-# ./build.sh krokysjezisem
+echo "Building Krokysjezisem"
+./build.sh krokysjezisem
 
-# echo "Starting server"
-# PORT=8000
-# rye run python -m http.server $PORT --directory dist &
-# SERVER_PID=$!
+echo "Starting server"
+PORT=8000
+rye run python -m http.server $PORT --directory dist &
+SERVER_PID=$!
 
-# echo "Running tests"
-# bunx playwright test tests/krokysjezisem.spec.js
+echo "Running tests"
+bunx playwright test tests/krokysjezisem.spec.js
 
-# echo "Killing server"
-# kill $SERVER_PID
+echo "Killing server"
+kill $SERVER_PID
--- tests/krokysjezisem.spec.js
+++ tests/krokysjezisem.spec.js
@@ -3,7 +3,7 @@ import { describe, test, expect } from '@playwright/test';
 const viewportDesktop = { width: 1440, height: 900 };
 const viewportMobile = { width: 390, height: 844 };
 
-describe('discovery', () => {
+describe('bozi rodina', () => {
   const link = 'http://localhost:8000/';
   test('desktop', async ({ page }) => {
     await page.setViewportSize(viewportDesktop);
@@ -30,8 +30,8 @@ describe('discovery', () => {
   });
 });
 
-describe('jak-zacit-discovery', () => {
-  const link = 'http://localhost:8000/jak-zacit-discovery/';
+describe('posileni-vize', () => {
+  const link = 'http://localhost:8000/posileni-vize/';
   test('desktop', async ({ page }) => {
     await page.setViewportSize(viewportDesktop);
     await page.goto(link);
@@ -49,54 +49,16 @@ describe('jak-zacit-discovery', () => {
   });
 });
 
-describe('texty-ke-cteni', () => {
-  const link = 'http://localhost:8000/texty-ke-cteni/';
-  test('desktop', async ({ page }) => {
-    await page.setViewportSize(viewportDesktop);
-    await page.goto(link);
-    await expect(page).toHaveScreenshot();
-    await page.click('label[title="Tmavý režim"]');
-    await expect(page).toHaveScreenshot();
-  });
-
-  test('mobile', async ({ page }) => {
-    await page.setViewportSize(viewportMobile);
-    await page.goto(link);
-    await expect(page).toHaveScreenshot();
-    await page.click('label[title="Tmavý režim"]');
-    await expect(page).toHaveScreenshot();
-  });
-});
-
-describe('karticky-k-tisku', () => {
-  const link = 'http://localhost:8000/karticky-k-tisku/';
-  test('desktop', async ({ page }) => {
-    await page.setViewportSize(viewportDesktop);
-    await page.goto(link);
-    await expect(page).toHaveScreenshot();
-    await page.click('label[title="Tmavý režim"]');
-    await expect(page).toHaveScreenshot();
-  });
-
-  test('mobile', async ({ page }) => {
-    await page.setViewportSize(viewportMobile);
-    await page.goto(link);
-    await expect(page).toHaveScreenshot();
-    await page.click('label[title="Tmavý režim"]');
-    await expect(page).toHaveScreenshot();
-  });
-});
-
-describe('discovery print', () => {
-  test('page 1: discovery', async ({ page }) => {
+describe('bozi-rodina print', () => {
+  test('page 1: bozi-rodina', async ({ page }) => {
     await page.setViewportSize(viewportDesktop);
     await page.goto('http://localhost:8000/#print');
     await expect(page).toHaveScreenshot({});
   });
 
-  test('page 2: jak zacit discovery', async ({ page }) => {
+  test('page 2: posileni-vize', async ({ page }) => {
     await page.setViewportSize(viewportDesktop);
-    await page.goto('http://localhost:8000/jak-zacit-discovery-print/#print');
+    await page.goto('http://localhost:8000/posileni-vize/#print');
     await expect(page).toHaveScreenshot();
   });
 });
